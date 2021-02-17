01a1b06bd968a2957196d0d354aff86efd650a75
Droissart Nicolas <nicodrg@live.com>
2021-02-17 21:39:25 +0100

    massive update from local work

--- index.js
+++ index.js
@@ -2,23 +2,57 @@ import fastify from 'fastify';
 // see axios doc on how to use it
 import axios from 'axios';
 
-const app = fastify({ logger: true });
+const app = fastify({logger: true});
+
+const getCatFacts = async (catFactsAmount) => {
+    try {
+        return (await axios.get(`https://cat-fact.herokuapp.com/facts/random?amount=${catFactsAmount}`)).data
+            .map(fact => fact.text)
+    } catch (e) {
+        return null
+    }
+}
+
+const getFoxImage = async () => {
+    try {
+        return (await axios.get('https://randomfox.ca/floof/')).data.image
+    } catch (e) {
+        return null
+    }
+}
+
+const getHolidays = async (countryCode) => {
+    try {
+        return (await axios.get(`https://date.nager.at/api/v2/PublicHolidays/2021/${countryCode}`)).data
+    } catch (e) {
+        return null
+    }
+}
 
 app.get('/', async (req, res) => {
-  return {
-    message: `Welcome to Node Babel with ${
-      req.body?.testValue ?? 'no testValue'
-    }`,
-  };
+    return {
+        'foxPicture': await getFoxImage(),
+        'catFacts': await getCatFacts(3),
+        'holidays': await getHolidays(req.params.countryCode)
+    };
+});
+
+app.post('/', async (req, res) => {
+    return {
+        'foxPicture': await getFoxImage(),
+        'catFacts': await getCatFacts(3),
+        'holidays': await getHolidays(req.body.countryCode)
+    };
 });
 
 // Run the server!
 const start = async () => {
-  try {
-    await app.listen(5000);
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await app.listen(5000);
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
 };
+
 start();
